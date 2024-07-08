10d4ab91b9c53b5b7ede8bc3331f56af02b0c16b
LoggyDoggy24 <george199207@mail.ru>
2024-07-08 23:22:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+Number_of_completed_homeworks = "12"
+print(Number_of_completed_homeworks)
+Number_of_spent_hours = "1.5"
+print(Number_of_spent_hours)
+Course_name = "Python"
+Time_per_one_task = "0.125"
+print("Course:", Course_name, ",", "total tasks:", Number_of_completed_homeworks, ",", "hours spent:", Number_of_spent_hours, ",", "average time of completion:", Time_per_one_task, ".")
